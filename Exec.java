d4318b955516f1cb15ef7ea97d905ec932f99b61
Hulkiniano <hulkiniano@gmail.com>
2025-04-24 10:25:50 +0200

    Exec.java

--- Exec.java
+++ Exec.java
@@ -1,10 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Fotoshop;
-
-import Fotoshop.Imagen;
 import java.io.*;
 import java.util.*;
 import java.util.logging.Level;
