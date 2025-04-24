965d16e0a21e866c80207c48cc743fa994a8f877
Hulkiniano <hulkiniano@gmail.com>
2025-04-24 10:25:06 +0200

    Imagen.java

--- Imagen.java
+++ Imagen.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Fotoshop;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
